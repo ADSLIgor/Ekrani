3d69d0e77c8c9612d4418d4490a0e345ddad4828
ADSLIgor <ADSLIgor@mail.ru>
2020-11-11 22:45:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/project7/FirstActivity.java
@@ -0,0 +1,27 @@
+package com.example.project7;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.project7.ui.main.SectionsPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+public class FirstActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.first_tab,
+                container, false );
+    }
+}
